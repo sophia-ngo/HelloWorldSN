fefb4855dedffbf23ceed5f29b3ee7a3c575d15a
gab <gabjpn@gabs-MacBook-Pro.local>
2020-01-27 15:29:09 -0700

    Changes were made to recieve input from user.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 	
 	public static void main(String[] args){
-		System.out.println("Hello, World!);
-		
+		Scanner name = new Scanner(System.in);
+
+		System.out.print("Enter your name: ");
+		String nameS = name.next();		// convert input into string
+
+		System.out.println("Hello, " + nameS + "\n");	// print input as string
+	}		
 }
